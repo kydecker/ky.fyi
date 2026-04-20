3713ea03feb966a499e57c4f17e2b87c8876c270
Eva Decker <itsevadecker@gmail.com>
2026-04-19 22:51:40 -0400

    More optimization

--- .nano-staged.js
+++
@@ -1,6 +0,0 @@
-export default {
-  "*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,json,jsonc,css,astro}":
-    "biome check --write",
-  "*.{png,jpg,gif,webp,heic,avif,mp4,mov,mkv,webm}": (api) =>
-    `optimo ${api.filenames.join(" ")}`,
-};
