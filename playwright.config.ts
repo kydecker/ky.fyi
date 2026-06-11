3b0eb51ca78c87ce57759c7a185e2df8e0e3ee13
Ky Decker <itsevadecker@gmail.com>
2026-06-10 21:40:23 -0400

    Update Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
+  testDir: "./src",
+  testMatch: "**/*.spec.ts",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined, // Opt out of parallel tests on CI.
+  workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
-    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL ?? "http://localhost:4321",
+    baseURL: "http://localhost:4321",
     trace: "on-first-retry",
   },
   projects: [
