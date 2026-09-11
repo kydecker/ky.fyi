07d7cdef2a814be00cb1256752a3ee9fc8664e3c
Timmy Cheung <siuwai0914@gmail.com>
2026-09-12 01:19:52 +0800

    change

--- astro.config.ts
+++ astro.config.ts
@@ -45,34 +45,36 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  fonts: [
-    {
-      provider: fontProviders.local(),
-      name: "HEX Franklin",
-      cssVariable: "--font-sans",
-      fallbacks: ["sans-serif"],
-      options: {
-        variants: [
-          {
-            src: ["./src/fonts/HEX_Franklin_v0.3_Variable.woff2"],
-          },
-        ],
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.local(),
+        name: "HEX Franklin",
+        cssVariable: "--font-sans",
+        fallbacks: ["sans-serif"],
+        options: {
+          variants: [
+            {
+              src: ["./src/fonts/HEX_Franklin_v0.3_Variable.woff2"],
+            },
+          ],
+        },
       },
-    },
-    {
-      provider: fontProviders.local(),
-      name: "MonoLisa",
-      cssVariable: "--font-mono",
-      fallbacks: ["monospace"],
-      options: {
-        variants: [
-          {
-            src: ["./src/fonts/MonoLisaVariableNormal.woff2"],
-          },
-        ],
+      {
+        provider: fontProviders.local(),
+        name: "MonoLisa",
+        cssVariable: "--font-mono",
+        fallbacks: ["monospace"],
+        options: {
+          variants: [
+            {
+              src: ["./src/fonts/MonoLisaVariableNormal.woff2"],
+            },
+          ],
+        },
       },
-    },
-  ],
+    ],
+  },
   redirects: {
     "/garden": "/",
     "/projects": "/",
