bd81faa57a4b1f16f8fbf0eaf817e969c46d95c2
Ky Decker <hi@ky.fyi>
2026-09-22 12:33:38 -0400

    New about page (#891)

--- src/constants/site-info.ts
+++ src/constants/site-info.ts
@@ -9,10 +9,10 @@ export const siteInfo = {
   rssTitle: "Dispatches from Ky Decker",
   fathomId: "WKFTECUZ",
   email: "hi@ky.fyi",
-  socials: [
-    { name: "Are.na", href: "https://www.are.na/ky-decker/" },
-    { name: "GitHub", href: "https://github.com/kydecker" },
-    { name: "LinkedIn", href: "https://linkedin.com/in/kyfyi" },
-    { name: "RSS", href: "/rss.xml" },
-  ] satisfies SocialLink[],
+  socials: {
+    arena: { name: "Are.na", href: "https://www.are.na/ky-decker/" },
+    github: { name: "GitHub", href: "https://github.com/kydecker" },
+    linkedin: { name: "LinkedIn", href: "https://linkedin.com/in/kyfyi" },
+    rss: { name: "RSS", href: "/rss.xml" },
+  } satisfies Record<string, SocialLink>,
 } as const;
