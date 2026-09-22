05ac8dfa3ded4325ad7bd6398156b1e9f0d58143
Ky Decker <itsevadecker@gmail.com>
2026-09-22 15:05:11 -0400

    Social links in footer

--- src/constants/site-info.ts
+++ src/constants/site-info.ts
@@ -1,6 +1,9 @@
+import type { SocialIconName } from "../components/Footer/SocialIcon.astro";
+
 export type SocialLink = {
   name: string;
   href: string;
+  icon: SocialIconName;
 };
 
 export const siteInfo = {
@@ -10,9 +13,27 @@ export const siteInfo = {
   fathomId: "WKFTECUZ",
   email: "hi@ky.fyi",
   socials: {
-    arena: { name: "Are.na", href: "https://www.are.na/ky-decker/" },
-    github: { name: "GitHub", href: "https://github.com/kydecker" },
-    linkedin: { name: "LinkedIn", href: "https://linkedin.com/in/kyfyi" },
-    rss: { name: "RSS", href: "/rss.xml" },
+    email: { name: "Email", href: "mailto:hi@ky.fyi", icon: "mail" },
+    arena: {
+      name: "Are.na",
+      href: "https://www.are.na/ky-decker/",
+      icon: "arena",
+    },
+    glass: {
+      name: "Glass",
+      href: "https://glass.photo/kydecker",
+      icon: "glass",
+    },
+    github: {
+      name: "GitHub",
+      href: "https://github.com/kydecker",
+      icon: "github",
+    },
+    linkedin: {
+      name: "LinkedIn",
+      href: "https://linkedin.com/in/kyfyi",
+      icon: "linkedin",
+    },
+    rss: { name: "RSS", href: "/rss.xml", icon: "rss" },
   } satisfies Record<string, SocialLink>,
 } as const;
