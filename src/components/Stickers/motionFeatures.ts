95a9b2ddcb8e15280f9f95b1960e50e495ee1b05
Ky Decker <itsevadecker@gmail.com>
2026-09-23 01:24:56 -0400

    Lazy load motion features

---
+++ src/components/Stickers/motionFeatures.ts
@@ -0,0 +1,2 @@
+// Loaded on demand by LazyMotion to keep animation features out of the initial bundle
+export { domMax as default } from "motion/react";
--- src/components/Subscribe/Dialogue/Dialogue.tsx
+++ src/components/Subscribe/Dialogue/Dialogue.tsx
@@ -1,10 +1,13 @@
 import "./dialogue.css";
 
-import { AnimatePresence, domAnimation, LazyMotion } from "motion/react";
+import { AnimatePresence, LazyMotion } from "motion/react";
 import React from "react";
 
 import { DialogueBubble } from "./DialogueBubble";
 import { Emote, type EmoteType } from "./Emote";
+
+const loadFeatures = () =>
+  import("./motionFeatures").then((res) => res.default);
 
 interface DialogueProps {
   text: string | null;
@@ -16,7 +19,7 @@ export const Dialogue = React.memo(
   ({ text, emote, onEmoteClick }: DialogueProps) => {
     return (
       <div className="dialogueContainer">
-        <LazyMotion features={domAnimation} strict>
+        <LazyMotion features={loadFeatures} strict>
           <AnimatePresence>
             <DialogueBubble text={text} key="bubble" />
             <Emote emote={emote} key="emote" onEmoteClick={onEmoteClick} />
