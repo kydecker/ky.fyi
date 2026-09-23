2f5bab166f4fa265217388a98166ca05f5186e65
Ky Decker <itsevadecker@gmail.com>
2026-09-23 01:16:27 -0400

    Inline stylesheets

--- astro.config.ts
+++ astro.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
   trailingSlash: "never",
   build: {
     format: "file",
+    inlineStylesheets: "always",
   },
   adapter: cloudflare({
     prerenderEnvironment: "node",
